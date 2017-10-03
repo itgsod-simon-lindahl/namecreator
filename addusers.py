8df12d80f1efc1a81cc526aefa73e9aba322f940
bo johansson <bo.l.johansson@gmail.com>
2017-10-03 22:57:29 +0200

    rewrite adduser

--- addusers.py
+++ addusers.py
@@ -1,14 +1,24 @@
+import os
 from nick import create_nick
 
+# remove usernicks.csv if it exist
+os.remove('usernicks.csv')
+
+# open user csv file
 with open('users.csv') as userfile:
     # read the lines
     for row in userfile.readlines():
+        # split line on ","
         user = row.strip().split(',')
-
+        # assign values
         firstname = user[0]
-        lastname = user [1]
+        lastname = user[1]
 
+        # call create_nick to build a username
         nickname = create_nick(firstname, lastname)
 
-        print nickname
+        # write user and nickname to file,
 
+        with open('usernicks.csv', 'a') as usernicksfile:
+            line = firstname + ',' + lastname + ',' + nickname + '\n'
+            usernicksfile.write(line)
